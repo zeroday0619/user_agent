03b0154d7019ddc8e4323a07f106a383cbd8af22
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-07-28 11:20:19 +0900

    feat: Modify data file to be imported from cdn

--- user_agent/device.py
+++ user_agent/device.py
@@ -1,9 +1,10 @@
-import os.path
-import json
+import requests
 
+def fetch_device_data(url: str):
+    resp = requests.get(url)
+    return resp.json()
 
-PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
-SMARTPHONE_DEV_IDS = json.load(open(os.path.join(
-    PACKAGE_DIR, 'data/smartphone_dev_id.json')))
-TABLET_DEV_IDS = json.load(open(os.path.join(
-    PACKAGE_DIR, 'data/tablet_dev_id.json')))
+SMARTPHONE_DEV_IDS = fetch_device_data('https://cdn.statically.io/gh/zeroday0619/user_agent/master/user_agent/data/smartphone_dev_id.json')
+SMARTPHONE_DEV_EXT = fetch_device_data('https://cdn.statically.io/gh/zeroday0619/user_agent/master/user_agent/data/smartphone_dev_ext.json')
+TABLET_DEV_IDS = fetch_device_data('https://cdn.statically.io/gh/zeroday0619/user_agent/master/user_agent/data/tablet_dev_id.json')
+TABLET_DEV_EXT = fetch_device_data('https://cdn.statically.io/gh/zeroday0619/user_agent/master/user_agent/data/tablet_dev_ext.json')
